39aeea85e271f302f085ec78ce7b20d266f907ac
tomatoonion <101053968+tomatoonion@users.noreply.github.com>
2023-04-13 12:44:35 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "database-1.cc7t5paqsavh.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "tanjacqueline-employee"
+customregion = "us-east-1"
